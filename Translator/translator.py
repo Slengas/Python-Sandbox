10e2d7cb36bf2aa48959b2743377cce33c75f682
Slengas <slengas@gmail.com>
2019-10-20 14:00:22 +0200

    added functionality to translator

--- Translator/translator.py
+++ Translator/translator.py
@@ -1,15 +1,18 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 
 def get_translation(w):
-    """
-    This function gets translation.
-    """
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        confirmation = input(
+            f"Did you mean {get_close_matches(w, data.keys())[0]} instead? [Y/n]: ").lower()
+        if confirmation == "y":
+            return data[get_close_matches(w, data.keys())[0]]
     else:
         return "The word is not in a dictionary."
 
@@ -17,4 +20,3 @@ def get_translation(w):
 word = input("Enter a word: ")
 
 print(get_translation(word))
-print("Additional information 23 ")
